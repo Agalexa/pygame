174efaa38dc2592b7b9acb439375dc2b5f76aa84
Agalexa <60199755+Agalexa@users.noreply.github.com>
2020-04-20 13:24:43 -0700

    Add files via upload

---
+++ Pygame/spaceinvaders.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+
+
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+
+
+#GAME LOOP
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill((255,0,0))
+    pygame.display.update()
